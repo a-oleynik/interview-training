af2dffd756f64673825056e856d17f7f7b1c697f
Andrey Oleynik <andrei.oleynik@gmail.com>
2024-11-25 02:24:54 +0100

    Add solution for the Check if Even Number task

--- src/main/java/com/oleynik/interviewtraining/lesson2/Lesson22.java
+++ src/main/java/com/oleynik/interviewtraining/lesson2/Lesson22.java
@@ -6,10 +6,9 @@ public class Lesson22 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n%2==0){
+        if (n % 2 == 0) {
             System.out.println("Even");
-        }
-        else {
+        } else {
             System.out.println("Odd");
         }
     }
---
+++ src/main/java/com/oleynik/interviewtraining/lesson2/ifswitch/CheckIfEven.java
@@ -0,0 +1,16 @@
+package com.oleynik.interviewtraining.lesson2.ifswitch;
+
+import java.util.Scanner;
+
+public class CheckIfEven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число: ");
+        int n = sc.nextInt();
+        if (n % 2 == 0) {
+            System.out.println("Чётное");
+        } else {
+            System.out.println("Нечётное");
+        }
+    }
+}
